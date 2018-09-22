4a38844206dbbfd92a21b2fd4f1e3880ed05d6d7
matthewdias <matthewdias@me.com>
2018-09-22 15:46:04 -0500

    Handle nulls

--- Contents/Code/update.py
+++ Contents/Code/update.py
@@ -46,23 +46,23 @@ def update_anime(type, metadata, media, force):
     if metadata.genres is None or force:
         metadata.genres = map(lambda c: c['attributes']['title'], includes['categories'])
 
-    if metadata.duration is None or force:
+    if (metadata.duration is None or force) and anime['episodeLength'] is not None:
         metadata.duration = anime['episodeLength'] * 60000
 
     if (metadata.rating is None or force) and anime['averageRating'] is not None:
         metadata.rating = float(anime['averageRating']) / 10
 
-    if metadata.title is None or force:
+    if (metadata.title is None or force) and anime['canonicalTitle'] is not None:
         metadata.title = anime['canonicalTitle']
 
-    if metadata.summary is None or force:
+    if (metadata.summary is None or force) and anime['synopsis'] is not None:
         metadata.summary = anime['synopsis']
 
-    if metadata.originally_available_at is None or force:
+    if (metadata.originally_available_at is None or force) and anime['startDate'] is not None:
         start_date = datetime.strptime(anime['startDate'], '%Y-%m-%d')
         metadata.originally_available_at = start_date
 
-    if metadata.content_rating is None or force:
+    if (metadata.content_rating is None or force) and anime['ageRatingGuide'] is not None:
         metadata.content_rating = anime['ageRatingGuide']
 
     if metadata.studio is None or force:
@@ -88,16 +88,20 @@ def update_anime(type, metadata, media, force):
                         people = filter(lambda p: p['id'] == person_id, includes['people'])
                         if people is not None:
                             person = people[0]
-                            locale = voice['attributes']['locale']
-                            locale = VOICE_LANGUAGES.get(locale, locale)
 
                             role = metadata.roles.new()
                             role.name = person['attributes']['name']
                             if person['attributes']['image'] is not None:
                                 role.photo = person['attributes']['image']['original']
-                            role.role = '{} ({})'.format(
-                                character['attributes']['canonicalName'], locale
-                            )
+
+                            if voice['attributes']['locale'] is not None:
+                                locale = voice['attributes']['locale']
+                                locale = VOICE_LANGUAGES.get(locale, locale)
+                                role.role = '{} ({})'.format(
+                                    character['attributes']['canonicalName'], locale
+                                )
+                            else:
+                                role.role = character['attributes']['canonicalName']
 
         for staff in includes['mediaStaff']:
             person_id = staff['relationships']['person']['data']['id']
@@ -109,9 +113,10 @@ def update_anime(type, metadata, media, force):
                 role.name = person['attributes']['name']
                 if person['attributes']['image'] is not None:
                     role.photo = person['attributes']['image']['original']
-                role.role = staff['attributes']['role']
+                if person['attributes']['role'] is not None:
+                    role.role = staff['attributes']['role']
 
-    if metadata.posters is None or force:
+    if (metadata.posters is None or force) and anime['posterImage'] is not None:
         poster_image = anime['posterImage']
         thumbnail = Proxy.Preview(HTTP.Request(
             poster_image['tiny'], immediate = True
@@ -119,7 +124,7 @@ def update_anime(type, metadata, media, force):
         metadata.posters[poster_image['large']] = thumbnail
 
     if type == 'tv':
-        if metadata.banners is None or force:
+        if (metadata.banners is None or force) and anime['coverImage'] is not None:
             cover_image = anime['coverImage']
             thumbnail = Proxy.Preview(HTTP.Request(
                 cover_image['original'],
@@ -133,7 +138,7 @@ def update_anime(type, metadata, media, force):
             update_collections(media, metadata, includes['mappings'])
 
     if type == 'movie':
-        if metadata.year is None or force:
+        if (metadata.year is None or force) and anime['startDate'] is not None:
             metadata.year = int(anime['startDate'][:4])
 
 def update_episodes(media, metadata, force, inc_episodes):
@@ -147,17 +152,17 @@ def update_episodes(media, metadata, force, inc_episodes):
         if episodes is not None:
             ep = episodes[0]['attributes']
 
-            if episode.title is None or force:
+            if (episode.title is None or force) and ep['canonicalTitle'] is not None:
                 episode.title = ep['canonicalTitle']
 
-            if episode.summary is None or force:
+            if (episode.summary is None or force) and ep['synopsis'] is not None:
                 episode.summary = ep['synopsis']
 
-            if episode.originally_available_at is None or force:
+            if (episode.originally_available_at is None or force) and ep['airdate'] is not None:
                 air_date = datetime.strptime(ep['airdate'], '%Y-%m-%d')
                 episode.originally_available_at = air_date
 
-            if episode.thumbs is None or force:
+            if (episode.thumbs is None or force) and ep['thumbnail'] is not None:
                 thumb_image = ep['thumbnail']['original']
                 thumbnail = Proxy.Preview(HTTP.Request(thumb_image, immediate = True).content)
                 episode.thumbs[thumb_image] = thumbnail
